36c60bd0b59ddc8adaa238168de807e81d77d117
Eugenius1st <eugenius1st@gmail.com>
2022-10-18 17:07:44 +0900

    [build] 빌드해보기

--- to-do-list/src/Pages/Edit.js
+++ to-do-list/src/Pages/Edit.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { useLocation, useNavigate, useParams } from "react-router-dom"; // 추가된 부분
