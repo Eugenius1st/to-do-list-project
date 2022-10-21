ec8b3ec544df49faea0b9eb166494ebdab3a2a6b
Eugenius1st <eugenius1st@gmail.com>
2022-10-21 17:45:36 +0900

    [delete]: 필요 없는 폴더 삭제

--- CodeStates/fe-sprint-cli-practice-main/getListMultiplesOfTwo.js
+++
@@ -1,8 +0,0 @@
-const { range } = require("range"); // range 모듈을 불러옵니다
-
-function getListMultiplesOfTwo(upTo) {
-  var range = require("range");
-  return range.range(2, upTo + 2, 2);
-}
-
-module.exports = getListMultiplesOfTwo;
